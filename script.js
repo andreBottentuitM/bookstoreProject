f3fe2ce02193c6bb68d5fdfed19ed4c5bb42c4bb
Andre Bottentuit <andre.bottentuit@gmail.com>
2022-08-29 22:40:25 -0300

    Input Search

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const transparency = document.querySelector('.transparent')
 closeButton.addEventListener('click', closePromotion)
 
 
-function closePromotion(e){
+function closePromotion(e) {
   closeButton.style.display = 'none'
   transparency.style.display = 'none'
   document.querySelector('.container-one').style.display = 'none'
@@ -61,7 +61,7 @@ let limitMonth = limitData.getMonth()
 limitMonth = month[limitMonth]
 
 //PUTTING THE LIMIT
-deadline.textContent = `até ${limitDay}/${limitMonth}/${limitYear}.`;
+deadline.textContent = limitDay >= 10 ? `até ${limitDay}/${limitMonth}/${limitYear}.` : `até 0${limitDay}/${limitMonth}/${limitYear}.`;
 
 const limitMilliseconds = limitData.getTime()
 
@@ -84,7 +84,6 @@ function takeTheTime() {
   let secondsRemaining = Math.floor((timeLeft % minuteInMilliseconds) / secondInMilliseconds)
 
   let dateFormatArray = [hoursRemaining, minutesRemaining, secondsRemaining]
-  console.log(dateFormatArray)
 
   day.textContent = daysRemaining
 
@@ -114,4 +113,57 @@ navToggle.addEventListener("click", function () {
 
 
   links.classList.toggle("expand");
+});
+
+
+//SEARCH WITH CLICK
+const firstLupa = document.querySelector('[lupa]')
+firstLupa.addEventListener('click', clickSearch)
+
+const secondLupa = document.querySelector('[secondLupa]')
+secondLupa.addEventListener('click', clickSearchResponsive)
+
+function clickSearch() {
+  const input = document.querySelector('[search]')
+  const textInput = input.value.toLowerCase().normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+  const valueOfAttribute = document.querySelector("#books")
+  for (di of valueOfAttribute.children) {
+    if (di.getAttribute("name").includes(textInput))
+      di.style.display = 'block'
+    else
+      di.style.display = "none"
+  }
+}
+
+function clickSearchResponsive() {
+  const input = document.querySelector('[searchResponsive]')
+  const textInput = input.value.toLowerCase().normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, '')
+  console.log(textInput)
+  const valueOfAttribute = document.querySelector("#books")
+  for (di of valueOfAttribute.children) {
+    if (di.getAttribute("name").includes(textInput))
+      di.style.display = 'block'
+    else
+      di.style.display = "none"
+  }
+}
+
+
+//SEARCH WITH ENTER
+const input = document.querySelector('[search]')
+input.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+
+    clickSearch();
+  }
+});
+
+const inputResponsive = document.querySelector('[searchResponsive]')
+inputResponsive.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault()
+
+    clickSearchResponsive();
+  }
 });
