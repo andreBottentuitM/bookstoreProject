4b38105adde44bdd27de992fe971456caeeebba7
André Bottentuit de Macedo <andre.bottentuit@gmail.com>
2022-10-13 13:33:11 -0300

    improving the form's validation

--- JAVASCRIPT/registration.js
+++ JAVASCRIPT/registration.js
@@ -3,48 +3,47 @@ const addressInput = document.querySelector('#street')
 const neighborhoodInput = document.querySelector('#neighborhood')
 const cityInput = document.querySelector('#city')
 const regionInput = document.querySelector('#region')
+const buttonSign = document.querySelector('#submit')
 
 //Patterns to inputs
 const masks = {
-    cpf (value) {
-        console.log(value)
+    cpf(value) {
         return value
-        .replace(/\D/g, '')
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d)/, '$1.$2')
-        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+            .replace(/\D/g, '')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d)/, '$1.$2')
+            .replace(/(\d{3})(\d{1,2})/, '$1-$2')
     },
-    phone (value) {
+    phone(value) {
         return value
-        .replace(/\D/g, '')
-        .replace(/(\d{2})(\d)/, '($1) $2')
-        .replace(/(\d{4})(\d)/, '$1-$2')
-        .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
-        .replace(/(-\d{5})\d+?$/, '$1')
+            .replace(/\D/g, '')
+            .replace(/(\d{2})(\d)/, '($1) $2')
+            .replace(/(\d{4})(\d)/, '$1-$2')
+            .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+            .replace(/(-\d{5})\d+?$/, '$1')
     },
     cep(value) {
         return value
-        .replace(/\D/g, '')
-        .replace(/(\d{5})(\d)/, '$1-$2')
+            .replace(/\D/g, '')
+            .replace(/(\d{5})(\d)/, '$1-$2')
     }
 }
 
-
-document.querySelectorAll('input').forEach($input => {
-   const field = $input.dataset.js
-
-   $input.addEventListener('input', (e) => {
-    e.target.value = masks[field](e.target.value)
-    console.log(e.target.value.length)
-   }, false)
+document.querySelectorAll('input').forEach(input => {
+    const field = input.dataset.js
+    input.addEventListener('input', (e) => {
+        e.target.value = masks[field](e.target.value)
+    }, false)
 })
 
-cepInput.addEventListener("keyup", (e) =>{
+cepInput.addEventListener("keyup", (e) => {
     const inputValue = e.target.value.replace('-', '')
-    if(inputValue.length === 8) {
-        getAddress(inputValue)
+        if (inputValue.length === 8) {
+            getAddress(inputValue)
+           
+        }
     }
-})
+)
 
 const getAddress = async (cep) => {
     cepInput.blur()
@@ -55,19 +54,91 @@ const getAddress = async (cep) => {
 
     const data = await response.json()
 
-    if(data.erro === 'true'){
+    if (data.erro === 'true') {
         addressInput.value = ''
         cityInput.value = ''
         neighborhoodInput.value = ''
         regionInput.value = ''
-
-    }else{
-    addressInput.value = data.logradouro
-    cityInput.value = data.localidade
-    neighborhoodInput.value = data.bairro
-    regionInput.value = data.uf}
-
+        document.querySelector('[cepText]').innerHTML = 'Carregando...'
+        setTimeout(() => document.querySelector('[cepText]').innerHTML = 'CEP incorreto', 1000)
+        setTimeout(() => {
+            document.querySelector('[cepText]').innerHTML = ''
+            cepInput.value = ''
+            takeOffDisabled('wrong')
+        }, 2500)
+    
+    } else {
+        document.querySelector('[cepText]').innerHTML = 'Carregando...'
+        setTimeout(() => {
+            addressInput.value = data.logradouro
+            cityInput.value = data.localidade
+            neighborhoodInput.value = data.bairro
+            regionInput.value = data.uf
+            document.querySelector('[cepText]').innerHTML = ''
+            takeOffDisabled('correct')
+        }, 1000)
+    }
+   return data
 }
 
+function takeOffDisabled(item) {
+    const inputsWithDisabled = document.querySelectorAll('.validation')
+    inputsWithDisabled.forEach(input => {
+       
+        if (cepInput.value != '' && item == 'correct') {
+            input.removeAttribute('disabled')
+        } else if (item == 'wrong') {
+            input.setAttribute('disabled', 'disabled')
+        }
+    })
+}
 
+const inputs = document.querySelectorAll('input')
+inputs.forEach(input => input.addEventListener('keypress', verificationInput))
+inputs.forEach(input => input.addEventListener('keyup', verificationInput))
 
+    function verificationInput(e) {
+        errors.forEach((item) => {
+            console.log(item)
+            if (e.target.id == item.input) {
+                item.function(e.target.value)
+            }
+        })
+    }
+
+const confirmRegistration = async (e) => {
+    e.preventDefault()
+
+    /*try{const apiUrl = `https://viacep.com.br/ws/${cepInput.value}/json/`
+
+    const response = await fetch(apiUrl)
+
+    const data = await response.json()}
+    catch(error){console.log(error)}*/
+    const allErrors = document.querySelectorAll('p')
+    const allErrorsToArray = Array.prototype.slice.call(allErrors)
+    const allInputsToArray = Array.prototype.slice.call(inputs)
+    const resultForm = document.querySelector('.resultForm')
+    let errorsValidation = allErrorsToArray.every(item => {
+       return item.innerHTML == ''
+    })
+ 
+    let inputsValidation = allInputsToArray.every(item => {
+        return item.value != ''
+    })
+
+    
+        if(errorsValidation && inputsValidation){
+
+            resultForm.innerHTML = 'Cadastro realizado com sucesso!'
+            resultForm.style.color = 'green'
+            console.log('Sucesso!')
+        }else{
+            resultForm.innerHTML = 'Cadastro realizado sem sucesso! Verifique se algo foi preenchido inadequadamente.'
+            resultForm.style.color = 'red'
+            console.log('Sem sucesso!')
+        }
+        
+}
+
+buttonSign.addEventListener('click', confirmRegistration)
---
+++ JAVASCRIPT/wrong-registration.js
@@ -0,0 +1,96 @@
+const errors = [{
+     input: 'name',
+     function: (text) => {
+         if(text.length < 3){
+           document.querySelector('[nameText]').innerHTML = 'Deve-se ter no mínimo 3 caracteres.'
+         }else{
+            document.querySelector('[nameText]').innerHTML = ""
+         }
+     }
+},
+  {
+    input: 'cpf',
+    function: (text) => {
+        if(text.length < 14) {
+            document.querySelector('[cpfText]').innerHTML = 'O CPF deve conter 11 dígitos.'
+        }else{
+            document.querySelector('[cpfText]').innerHTML = ""
+        }
+    }
+  },
+  {
+    input: 'data',
+    function: (text) => {
+        if(text.length < 10) {
+            document.querySelector('[dateText]').innerHTML = 'Data de nascimento incorreta.'
+        }else{
+            document.querySelector('[dateText]').innerHTML = ""
+        }
+    }
+  },
+  {
+    input: 'tel',
+    function: (text) => {
+        if(text.length < 15) {
+            document.querySelector('[phoneText]').innerHTML = 'O telefone deve ter o DDD mais 9 dígitos.'
+        }else{
+            document.querySelector('[phoneText]').innerHTML = ""
+        }
+    }
+  },
+  {
+    input: 'email',
+    function: (text) => {
+        const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        if(!emailRegex.test(text)) {
+            document.querySelector('[emailText]').innerHTML = 'O email está incorreto.'
+        }else{
+            document.querySelector('[emailText]').innerHTML = ""
+        }
+    }
+  },
+  {
+    input: 'confirm',
+    function: (text) => {
+        const password = document.querySelector('#password')
+        if(password.value != text) {
+            document.querySelector('[confirmText]').innerHTML = 'A confirmação de senha não confere.'
+        }else{
+            document.querySelector('[confirmText]').innerHTML = ""
+        }
+    }
+  },
+  {
+    input: 'password',
+    function: (text) => {
+        const confirm = document.querySelector('#confirm')
+        if(text.length < 6) {
+            document.querySelector('[passwordText]').innerHTML = 'Sua senha tem que ter no mínimo 6 dígitos'
+        }else{
+            document.querySelector('[passwordText]').innerHTML = ''
+        }
+        if((confirm.value != text.value && confirm.value.length != 0)){
+            document.querySelector('[confirmText]').innerHTML = "A confirmação de senha não confere."
+        }else{
+            document.querySelector('[confirmText]').innerHTML = ''
+        }
+    }
+  },
+  {
+    input: 'cep',
+    error: 'CEP inexistente.',
+    function: (text) => {
+        if(text.length < 9) {
+            takeOffDisabled('wrong')
+            document.querySelector('[cepText]').innerHTML = 'O CEP deve ter 8 dígitos.'
+            addressInput.value = ''
+        cityInput.value = ''
+        neighborhoodInput.value = ''
+        regionInput.value = ''
+        }else{
+            document.querySelector('[cepText]').innerHTML = ""
+        }
+    }
+  },
+]
+
